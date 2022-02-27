8d0100de595000ac5fa928c9e63bae81c6c5d70b
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2022-02-27 13:36:30 +0100

    Create refresh token

--- tuyadevice/tuyadevice.go
+++ tuyadevice/tuyadevice.go
@@ -6,14 +6,15 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
 
 type TokenResponse struct {
 	Result struct {
-		AccessToken  string `json:"access_token"`
-		ExpireTime   int    `json:"expire_time"`
-		RefreshToken string `json:"refresh_token"`
-		UID          string `json:"uid"`
+		AccessToken     string `json:"access_token"`
+		TokenExpireTime int    `json:"expire_time"`
+		RefreshToken    string `json:"refresh_token"`
+		UID             string `json:"uid"`
 	} `json:"result"`
 	Success bool  `json:"success"`
 	T       int64 `json:"t"`
@@ -24,38 +25,74 @@ type TokenRetriever interface {
 }
 
 type TuyaDevice struct {
-	Name         string
-	DeviceType   string
-	Host         string
-	ClientID     string
-	Secret       string
-	DeviceID     string
-	Token        string
-	ExpireTime   int
-	RefreshToken string
+	Name            string
+	DeviceType      string
+	Host            string
+	ClientID        string
+	Secret          string
+	DeviceID        string
+	Token           string
+	TokenExpireTime int64
+	RefreshToken    string
+}
+
+func (device *TuyaDevice) UpdateToken(client http.Client) error {
+
+	now := time.Now()
+	currentTimestamp := now.Unix()
+	if device.TokenExpireTime-currentTimestamp < 600 {
+		log.Println("Device " + device.Name + " token will expire in less than 10 secons, retrive new token.")
+		body := []byte(``)
+		req, _ := http.NewRequest("GET", device.Host+"/v1.0/token/"+device.RefreshToken, bytes.NewReader(body))
+
+		device.buildHeader(req, body)
+		resp, err := client.Do(req)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+		bs, _ := ioutil.ReadAll(resp.Body)
+		ret := TokenResponse{}
+		unmarshalErr := json.Unmarshal(bs, &ret)
+		if unmarshalErr != nil {
+			return unmarshalErr
+		}
+		log.Println("refresh token GET response:", string(bs))
+		device.Token = ret.Result.AccessToken
+		now := time.Now() // current local time
+		device.TokenExpireTime = now.Unix() + int64(ret.Result.TokenExpireTime)
+		device.RefreshToken = ret.Result.RefreshToken
+	}
+	return nil
 }
 
 func (device *TuyaDevice) RetrieveToken(client http.Client) error {
-	body := []byte(``)
-	req, _ := http.NewRequest("GET", device.Host+"/v1.0/token?grant_type=1", bytes.NewReader(body))
+	if device.TokenExpireTime == 0 { // New token
+		body := []byte(``)
+		req, _ := http.NewRequest("GET", device.Host+"/v1.0/token?grant_type=1", bytes.NewReader(body))
 
-	device.buildHeader(req, body)
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	bs, _ := ioutil.ReadAll(resp.Body)
-	ret := TokenResponse{}
-	unmarshalErr := json.Unmarshal(bs, &ret)
-	if unmarshalErr != nil {
-		return unmarshalErr
-	}
-	log.Println("token GET response:", string(bs))
-	device.Token = ret.Result.AccessToken
-	device.ExpireTime = ret.Result.ExpireTime
-	device.RefreshToken = ret.Result.RefreshToken
+		device.buildHeader(req, body)
+		resp, err := client.Do(req)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+		bs, _ := ioutil.ReadAll(resp.Body)
+		ret := TokenResponse{}
+		unmarshalErr := json.Unmarshal(bs, &ret)
+		if unmarshalErr != nil {
+			return unmarshalErr
+		}
+		log.Println("token GET response:", string(bs))
+		device.Token = ret.Result.AccessToken
+		now := time.Now() // current local time
+		device.TokenExpireTime = now.Unix() + int64(ret.Result.TokenExpireTime)
+		device.RefreshToken = ret.Result.RefreshToken
 
+	} else {
+		// refresh token
+		return device.UpdateToken(client)
+	}
 	return nil
 
 }
