ea60a2e7865973896b0ac295be5f01830f79e4e8
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2022-05-02 22:36:46 +0200

    TestGetToken

--- device_manager/device_manager_test.go
+++ device_manager/device_manager_test.go
@@ -1,52 +1,23 @@
 package devices
 
-//
-//import (
-//	"bytes"
-//	"io/ioutil"
-//	"net/http"
-//	"testing"
-//
-//	config "github.com/a-castellano/AlarmManager/config_reader"
-//)
-//
-//type RoundTripperMock struct {
-//	Response *http.Response
-//	RespErr  error
-//}
-//
-//func (rtm *RoundTripperMock) RoundTrip(*http.Request) (*http.Response, error) {
-//	return rtm.Response, rtm.RespErr
-//}
-//
-//func TestGetToken(t *testing.T) {
-//
-//	client := http.Client{Transport: &RoundTripperMock{Response: &http.Response{Body: ioutil.NopCloser(bytes.NewBufferString(`{"result":{"access_token":"testtoken","expire_time":7200,"refresh_token":"refesh","uid":"bay1635003708553hilW"},"success":true,"t":1644740470593}`))}}}
-//
-//	deviceFromConfig := config.TuyaDevice{Name: "Test", DeviceType: "99AST", Host: "https://test.windmaker.net", ClientID: "test", Secret: "test", DeviceID: "test"}
-//
-//	token, tokenError := GetToken(client, device)
-//
-//	if tokenError != nil {
-//		t.Errorf("Token retrievement should not fail. Error was %s", tokenError)
-//	}
-//
-//	if token != "testtoken" {
-//		t.Errorf("Retrived token should be testtoken, not %s.", token)
-//	}
-//
-//}
-//
-//func TestGetFailedToken(t *testing.T) {
-//
-//	client := http.Client{Transport: &RoundTripperMock{Response: &http.Response{Body: ioutil.NopCloser(bytes.NewBufferString(`{"result":{"access_token":"testtoken","expire_time":7200,"refresh_token":"refesh","uid":"bay1635003708553hilW","success":true,"t":1644740470593`))}}}
-//
-//	device := config.TuyaDevice{Name: "Test", DeviceType: "99AST", Host: "https://test.windmaker.net", ClientID: "test", Secret: "test", DeviceID: "test"}
-//
-//	_, tokenError := GetToken(client, device)
-//
-//	if tokenError == nil {
-//		t.Errorf("Token retrievement should fail.")
-//	}
-//
-//}
+import (
+	"os"
+	"testing"
+
+	config "github.com/a-castellano/AlarmManager/config_reader"
+)
+
+func TestGetToken(t *testing.T) {
+	os.Setenv("ALARM_MANAGER_CONFIG_FILE_LOCATION", "../config_reader/config_files_test/config_ok/")
+	devicesConfig, readConfigErr := config.ReadConfig()
+	if readConfigErr != nil {
+		t.Errorf("ReadConfig method without tuya devices should not fail, error was '%s'.", readConfigErr)
+	}
+	deviceConfig := devicesConfig.Devices["Home Alarm"]
+	device := CreateTuyaDeviceFromConfig(deviceConfig)
+
+	if device.DeviceID != "device123" {
+		t.Errorf("Processed device idshould be 'device123', not %s.", device.DeviceID)
+	}
+
+}
