98029e88c11a8644b015dc0eb178037b30c07df7
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2022-05-02 22:52:01 +0200

    Test AddDevice

--- device_manager/device_manager_test.go
+++ device_manager/device_manager_test.go
@@ -5,9 +5,10 @@ import (
 	"testing"
 
 	config "github.com/a-castellano/AlarmManager/config_reader"
+	tuyadevice "github.com/a-castellano/AlarmManager/tuyadevice"
 )
 
-func TestGetToken(t *testing.T) {
+func TestCreateTuyaDevice(t *testing.T) {
 	os.Setenv("ALARM_MANAGER_CONFIG_FILE_LOCATION", "../config_reader/config_files_test/config_ok/")
 	devicesConfig, readConfigErr := config.ReadConfig()
 	if readConfigErr != nil {
@@ -20,4 +21,35 @@ func TestGetToken(t *testing.T) {
 		t.Errorf("Processed device idshould be 'device123', not %s.", device.DeviceID)
 	}
 
+}
+
+func TestAddOneDevice(t *testing.T) {
+
+	deviceManager := DeviceManager{DevicesInfo: make(map[string]tuyadevice.Device), AlarmsInfo: make(map[string]Alarm)}
+	var device tuyadevice.TuyaDevice
+	device.Name = "Test Device"
+
+	deviceRef := &device
+	addErr := deviceManager.AddDevice(deviceRef)
+	if addErr != nil {
+		t.Errorf("AddDevice should not fail with only one device, error was '%s'.", addErr)
+	}
+}
+
+func TestAddTwoDevicesWithSameName(t *testing.T) {
+
+	deviceManager := DeviceManager{DevicesInfo: make(map[string]tuyadevice.Device), AlarmsInfo: make(map[string]Alarm)}
+	var device tuyadevice.TuyaDevice
+	device.Name = "Test Device"
+
+	var device2 tuyadevice.TuyaDevice
+	device2.Name = "Test Device"
+
+	deviceRef := &device
+	device2Ref := &device2
+	deviceManager.AddDevice(deviceRef)
+	addErr := deviceManager.AddDevice(device2Ref)
+	if addErr == nil {
+		t.Errorf("AddDevice should fail with two devices witch the same name.")
+	}
 }
