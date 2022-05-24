7876899387d7bf08d4c9369cb88576d4d8deb0a0
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2022-05-24 07:43:48 +0200

    Fix postinstall scripts

--- packaging/deb/scripts/postinstall.sh
+++ packaging/deb/scripts/postinstall.sh
@@ -5,5 +5,5 @@ mkdir -p /etc/windmaker-alarmmanager
 echo "### NOT starting on installation, please execute the following statements to configure windmaker-alarmmanager to start automatically using systemd"
 echo " sudo /bin/systemctl daemon-reload"
 echo " sudo /bin/systemctl enable windmaker-alarmmanager"
-echo "### You can start grafana-server by executing"
+echo "### You can start windmaker-alarmmanager by executing"
 echo " sudo /bin/systemctl start windmaker-alarmmanager"
