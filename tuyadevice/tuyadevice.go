d0b37b314d222ac53836955b03632ba0db7fd219
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2022-05-24 18:39:33 +0200

    Remove updateToken function

--- tuyadevice/tuyadevice.go
+++ tuyadevice/tuyadevice.go
@@ -73,43 +73,6 @@ func (device *TuyaDevice) Validate() error {
 	return nil
 }
 
-func (device *TuyaDevice) updateToken(client http.Client) error {
-
-	now := time.Now()
-	currentTimestamp := now.Unix()
-	log.Println(currentTimestamp)
-	log.Println(device.TokenExpireTime)
-	log.Println(device.TokenExpireTime - currentTimestamp)
-	if device.TokenExpireTime-currentTimestamp < 0 {
-		log.Println("Device " + device.Name + " token has expired, retrive new token.")
-		body := []byte(``)
-		req, _ := http.NewRequest("GET", device.Host+"/v1.0/token/"+device.RefreshToken, nil)
-
-		device.buildHeader(req, body)
-		resp, err := client.Do(req)
-		if err != nil {
-			return err
-		}
-		defer resp.Body.Close()
-		log.Println(resp.Body)
-		bs, _ := ioutil.ReadAll(resp.Body)
-		ret := TokenResponse{}
-		unmarshalErr := json.Unmarshal(bs, &ret)
-		if unmarshalErr != nil {
-			return unmarshalErr
-		}
-		log.Println("refresh token GET response:", string(bs))
-		device.Token = ret.Result.AccessToken
-		now := time.Now() // current local time
-		device.TokenExpireTime = now.Unix() + int64(ret.Result.TokenExpireTime)
-		device.RefreshToken = ret.Result.RefreshToken
-		log.Println("New token expire time: ", device.TokenExpireTime)
-	} else {
-		log.Println("Device " + device.Name + " token has not expired yet.")
-	}
-	return nil
-}
-
 func (device *TuyaDevice) RetrieveToken(client http.Client) error {
 	var retriveNewToken bool = false
 	if device.TokenExpireTime == 0 {
